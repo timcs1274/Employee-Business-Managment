089204cb616737cdbafa1b2d1b71e502a8124fa1
timcs <timcs1274@gmail.com>
2022-07-27 12:44:45 -0400

    added inquire prompts

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
+
+const promptUser = () => {
+    inquirer.prompt ([
+        {
+        type: 'list',
+        name: 'choiceSelector',
+        message: 'What would you like to do?',
+        choices: ['View all employees',
+                  'View all employees by Department',
+                  'View all employees by manager',
+                  'Add employee',
+                  'Remove employee',
+                  'Update employee role',
+                  'Update employee manager',
+                  'View all roles',
+                  'Add role',
+                  'View all departments',
+                  'Add department',
+                  'Remove department',
+                  'None',]
+        }
+    ])
+    
+}
