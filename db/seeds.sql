373428eb0fbbb58a5fd8fae66afee600cb4b5c64
timcs <timcs1274@gmail.com>
2022-07-27 12:15:12 -0400

    added employee into seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,3 +16,13 @@ VALUES
 ('Corporate Paralegal', 60,000, 4),
 ('Corporate Lawyer' 120,000, 4),
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Greg', 'Gregerson', 1, NULL),
+('Mark', 'Markerson', 2, 1),
+('Tim', 'Timerson', 3, NULL),
+('Daniel', 'Danielson', 4, 2),
+('Jerry', 'Jerrison', 5, NULL),
+('Bob', 'Boberson', 6, 3),
+('Christy', 'Christison', 7, NULL),
+('Megan', 'Meganson', 8, 4),
