b932c36ac5214c5bd08c0588f3116d3a67aeb59b
timcs <timcs1274@gmail.com>
2022-07-27 12:07:52 -0400

    added roles in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,4 +3,16 @@ VALUES
 ('IT')
 ('Accounting'),
 ('Sales'),
-('Legal')
+('Legal'),
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Junior Software Engineer', 70,000, 1),
+('Senior Software Engineer', 100,000, 1),
+('Accountant', 80,000, 2),
+('Accountant Intern' 45,000, 2),
+('Sales Development Rep' 60,000, 3),
+('Account Development Rep', 80,000, 3),
+('Corporate Paralegal', 60,000, 4),
+('Corporate Lawyer' 120,000, 4),
+
